e1a12e71b1944a9a7d0f1eebe3a3bf3a5ca3838c
KAZUYA215074 <nrtgntm@me.com>
2019-05-27 14:32:12 +0900

    マラソン一問目が終了したのでアップデートしました

--- src/main/java/com/example/controller/Marahon.java
+++ src/main/java/com/example/controller/Marahon.java
@@ -1,14 +1,24 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/marathon")
 public class Marahon {
 
+	
 	@RequestMapping("")
-	public Stiong index() {
+	public String index() {
 		return "inputname";
+	}
+	
+	@RequestMapping("/01")
+	public String Exe01(String name,Model model) {
+		
+		model.addAttribute("name",name);
+		
+		return "ex01";
 	}
 }
