8e206def3dc60f56713fd4293131bb136419f67c
KAZUYA215074 <nrtgntm@me.com>
2019-05-27 15:35:16 +0900

    15km,カンマ区切り以外終了したのでアップデートします

--- src/main/java/com/example/controller/Marahon.java
+++ src/main/java/com/example/controller/Marahon.java
@@ -1,24 +1,87 @@
 package com.example.controller;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.User;
 
 @Controller
 @RequestMapping("/marathon")
 public class Marahon {
 
-	
+//	@RequestMapping("")
+//	public String index() {
+//		return "inputname";
+//	}
+
+//	@RequestMapping("")
+//	public String index() {
+//		return "inputname2";
+//	}
+
+//	@RequestMapping("")
+//	public String index() {
+//		return "inputname3";
+//	}
+
 	@RequestMapping("")
 	public String index() {
-		return "inputname";
+		return "inputname4";
 	}
-	
+
 	@RequestMapping("/01")
-	public String Exe01(String name,Model model) {
-		
-		model.addAttribute("name",name);
-		
+	public String Exe01(String name, Model model) {
+
+		model.addAttribute("name", name);
+
 		return "ex01";
 	}
+
+	@RequestMapping("/02")
+	public String Exe02(User user, Model model) {
+
+		model.addAttribute("name", user);
+
+		return "ex02";
+	}
+
+	@Autowired
+	private HttpSession session;
+
+	@RequestMapping("/03")
+	public String Exe03(String num1, String num2) {
+		int answer = Integer.parseInt(num1) * Integer.parseInt(num2);
+
+		session.setAttribute("num1", num1);
+		session.setAttribute("num2", num2);
+		session.setAttribute("answer", answer);
+
+		return "ex03";
+	}
+
+	@RequestMapping("/03-02")
+	public String Exe03_02() {
+		return "ex03_02";
+	}
+
+	@Autowired
+	private ServletContext application;
+
+	@RequestMapping("/04")
+	public String Exe04(String goods1, String goods2, String goods3) {
+		
+		int answer=Integer.parseInt(goods1)+Integer.parseInt(goods2)+Integer.parseInt(goods3);
+		int tax =(int)(answer*1.08);
+		
+		application.setAttribute("answer", answer);
+		application.setAttribute("tax", tax);
+		
+		return "ex04";
+	}
+
 }
